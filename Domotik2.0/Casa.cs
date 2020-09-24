1845fc117067660c9651d83ac1511207d727b52f
osmar151 <62614167+osmar151@users.noreply.github.com>
2020-09-24 14:41:46 -0600

    programa gy

---
+++ Domotik2.0/Casa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+namespace Domotik2._0
+{
+    class Casa
+    {
+        private static SerialPort Arduino = new SerialPort("COM9", 9600);
+
+        public void EnviarCaracter(string dato)
+        {
+            Arduino.Open();
+            Arduino.Write(dato);
+            Arduino.Close();
+        }
+
+    }
+
+}
+
